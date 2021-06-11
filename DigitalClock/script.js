65dc6c4c42218e89c0d06a4ff9c359f71e47b1c5
younusraza909 <younusraza909@gmail.com>
2021-06-11 09:14:23 +0500

    Added Functionality to our app to get and set time

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -1,7 +1,34 @@
-const newDate = () => {
+const fetchTime = () => {
   const date = new Date();
 
-  console.log(date);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let am_pm = "AM";
+
+  if (hours > 12) {
+    hours -= 12;
+    am_pm = "PM";
+  }
+  if (hours == 0) {
+    hours = 12;
+    am_pm = "AM";
+  }
+
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  updateDOM(hours, minutes, seconds, am_pm);
 };
 
-newDate();
+const updateDOM = (hrs, min, sec, am_pm) => {
+  document.querySelector("#hours").innerHTML = hrs;
+  document.querySelector("#minutes").innerHTML = min;
+  document.querySelector("#seconds").innerHTML = sec;
+  document.querySelector("#am_pm").innerHTML = am_pm;
+};
+
+setInterval(() => {
+  fetchTime();
+}, 1000);
